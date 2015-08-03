dbed99a93206968b9faee230f1da52ce738e7a9c
Highway-Man <dunechaser@glamis.tk>
2015-08-03 12:51:51 -0700

    added copyright and statement

--- src/prossensorssetupwizard/Activator.java
+++ src/prossensorssetupwizard/Activator.java
@@ -1,3 +1,22 @@
+/*	
+ * 	Copyright 2015 Jacob Knaup
+ * 
+ * 	This file is part of PROS Sensors Setup Wizard.
+ * 
+ * 	PROS Sensors Setup Wizard is free software: you can redistribute it and/or modify
+ * 	it under the terms of the GNU General Public License as published by
+ * 	the Free Software Foundation, either version 3 of the License, or
+ * 	(at your option) any later version.
+ * 
+ * 	PROS Sensors Setup Wizard is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
+ * 	You should have received a copy of the GNU General Public License
+ * 	along with PROS Sensors Setup Wizard.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
 package prossensorssetupwizard;
 
 import org.eclipse.jface.resource.ImageDescriptor;
